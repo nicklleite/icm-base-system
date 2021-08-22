f76279312fc6b4df816a31cce3838ed58a328a5d
Nicholas Leite <nicklleite@gmail.com>
2021-08-22 18:37:03 -0300

    Starting the use of Service classes

---
+++ app/Services/CompanyService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Company;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class CompanyService {
+
+    // Creates a company
+    // Retrieves a company
+    // Updates a company
+    // Deletes a company
+    // Validate all the input
+    // Validate all the business logic
+    // 
+}
