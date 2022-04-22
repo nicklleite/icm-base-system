c5a438ab46e0f236b7b4a0f61e5125a9b55818f5
Nicholas Leite <nicklleite@gmail.com>
2022-04-22 03:58:23 -0300

    :new: Files from the user personal documents. (Let's see how policies works and how it will be useful for this).

---
+++ database/migrations/2022_04_22_033908_create_user_documents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('user_documents', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_documents');
+    }
+}
